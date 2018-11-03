bc88082c48294fd936f0783d61a43cb9c8c5525e
Martín Santibáñez <martin.santibanez.a@gmail.com>
2018-11-03 02:51:14 -0300

    Etiquetas. closes #3

--- taller1/pipelines.py
+++ taller1/pipelines.py
@@ -20,24 +20,39 @@ class MaxLengthPipeline(object):
 #Pipeline encargado de agregar tanto autor como las citas
 class SQLiteCitasPipeline(object):            
     def process_item(self, item, spider):
-        # print(item)
         ins_autor_query="insert into autor('nombre')  values('{0}')"
-        ins_cita_query="insert into cita(cita,id_autor) values('{0}',{1})"
-        sel_autor_query="select id, nombre from autor where nombre='{0}'"
-        val=False
+        sel_autor_query="select id from autor where nombre='{0}'"
+        ins_cita_query="insert into cita(cita,id_autor) values('{0}', {1})"
+        ins_etiqueta_query="insert into etiqueta('nombre') values('{0}')"
+        sel_etiqueta_query="select id from etiqueta where nombre='{0}'"
+        ins_cita_etiqueta_query="insert into cita_etiqueta('id_cita', 'id_etiqueta') values({0}, {1})"
         conn=sqlite3.connect('parte1.db')
         cursor=conn.cursor()
-
+        # Obtener id Autor o insertar
         cursor.execute(sel_autor_query.format(item['autor']))
         autor_db=cursor.fetchone()
-        if autor_db is None or len(autor_db) == 0:
+        if autor_db is None:
             cursor.execute(ins_autor_query.format(item['autor']))
             conn.commit()
-            id_autor = cursor.lastrowid
+            id_autor=cursor.lastrowid
         else:
-            id_autor = autor_db[0]
-            
+            id_autor=autor_db[0]
+        # Insertar Cita
         cursor.execute(ins_cita_query.format(item['cita'].replace("'","´"), id_autor))
         conn.commit()
+        id_cita=cursor.lastrowid
+        # Etiquetas
+        for etiqueta in item['etiquetas']:
+            cursor.execute(sel_etiqueta_query.format(etiqueta))
+            etiqueta_db=cursor.fetchone()
+            if etiqueta_db is None:
+                cursor.execute(ins_etiqueta_query.format(etiqueta))
+                conn.commit()
+                id_etiqueta=cursor.lastrowid
+            else:
+                id_etiqueta=etiqueta_db[0]
+            cursor.execute(ins_cita_etiqueta_query.format(id_cita, id_etiqueta))
+            conn.commit()
+
         conn.close()
         return item
